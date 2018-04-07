6b0e3d6f10516c4eeffc62be738d38a58b844d82
maximbu <maximbu@gmail.com>
2018-04-07 23:03:22 +0300

    Q353_PeskyPalindromes

---
+++ src/UVA/AdHoc/Palindrome/Q353_PeskyPalindromes.java
@@ -0,0 +1,47 @@
+package UVA.AdHoc.Palindrome;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+class Q353_PeskyPalindromes {
+  public static void main(String[] st) {
+    Q353_PeskyPalindromes p = new Q353_PeskyPalindromes();
+    p.solve();
+  }
+
+  private void solve() {
+    Scanner sc = new Scanner(in);
+    while (sc.hasNextLine()) {
+      String input = sc.nextLine().trim();
+      int result = solve(input);
+      out.println("The string '" + input + "' contains " + result + " palindromes.");
+    }
+  }
+
+  private int solve(String st) {
+    HashSet<String> palindromes = new HashSet<>();
+    for (int i = 0; i < st.length(); i++) {
+      for (int j = i+1; j < st.length(); j++) {
+        String sub = st.substring(i,j);
+        if(isPalindrome(sub)){
+          palindromes.add(sub);
+        }
+      }
+      String sub = st.substring(i);
+      if(isPalindrome(sub)){
+        palindromes.add(sub);
+      }
+    }
+    return palindromes.size();
+  }
+
+  private boolean isPalindrome(String sub) {
+    return IntStream.range(0, sub.length())
+        .allMatch(i -> sub.charAt(i) == sub.charAt(sub.length() - i - 1));
+  }
+
+}
