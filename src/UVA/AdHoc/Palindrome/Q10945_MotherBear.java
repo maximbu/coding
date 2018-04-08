aa9842696ea1be974bf3457750891effd2791982
maximbu <maximbu@gmail.com>
2018-04-09 00:18:06 +0300

    Q10945_MotherBear,Q11221_MagicSquarePalindromes

---
+++ src/UVA/AdHoc/Palindrome/Q10945_MotherBear.java
@@ -0,0 +1,47 @@
+package UVA.AdHoc.Palindrome;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+import java.util.Scanner;
+
+class Q10945_MotherBear {
+  public static void main(String[] st) {
+    Q10945_MotherBear p = new Q10945_MotherBear();
+    p.solve();
+  }
+
+  private void solve() {
+    Scanner sc = new Scanner(in);
+    while (true) {
+      String input = sc.nextLine();
+      if(input.equals("DONE")) return;
+      if(isPalindrome(input)) {
+        out.println("You won't be eaten!");
+      }
+      else{
+        out.println("Uh oh..");
+      }
+    }
+  }
+
+  private boolean isPalindrome(String st) {
+    int start = 0;
+    int end = st.length()-1;
+    char[] str = st.toCharArray();
+    while (start<end){
+      while (!Character.isLetter(str[start])&&(start<end)){
+        start++;
+      }
+      while (!Character.isLetter(str[end])&&(start<end)){
+        end--;
+      }
+      if(!Character.isLetter(str[start]) && !Character.isLetter(str[end])) return true;
+      if(Character.toLowerCase(str[start]) != Character.toLowerCase(str[end])) return false;
+      start++;
+      end--;
+    }
+    return true;
+  }
+
+}
---
+++ src/UVA/AdHoc/Palindrome/Q11221_MagicSquarePalindromes.java
@@ -0,0 +1,70 @@
+package UVA.AdHoc.Palindrome;
+
+import static java.lang.System.in;
+
+import java.util.Scanner;
+
+class Q11221_MagicSquarePalindromes {
+
+  public static void main(String[] st) {
+    Q11221_MagicSquarePalindromes p = new Q11221_MagicSquarePalindromes();
+    p.solve();
+  }
+
+  private void solve() {
+    Scanner sc = new Scanner(in);
+    int cases = Integer.valueOf(sc.nextLine());
+    int c = 0;
+    while (c++ < cases) {
+      String input = sc.nextLine().trim();
+      StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < input.length(); i++) {
+        if (Character.isLetter(input.charAt(i))) {
+          sb.append(Character.toLowerCase(input.charAt(i)));
+        }
+      }
+      int ans = magicSquareSize(sb.toString());
+      System.out.println("Case #" + c + ":");
+      System.out.println(ans > -1 ? ans : "No magic :(");
+    }
+  }
+
+  private int magicSquareSize(String s) {
+    int len = s.length();
+    int sqSize = (int) Math.sqrt(len);
+    char[][] sq = new char[sqSize][sqSize];
+    char[] chars = s.toCharArray();
+    int ind = 0;
+    for (int i = 0; i < sqSize; i++) {
+      for (int j = 0; j < sqSize; j++) {
+        sq[i][j] = chars[ind++];
+      }
+    }
+    StringBuilder sb = new StringBuilder();
+    for (int i = sqSize - 1; i >= 0; i--) {
+      for (int j = sqSize - 1; j >= 0; j--) {
+        sb.append(sq[i][j]);
+      }
+    }
+    if (!sb.toString().equals(s))
+      return -1;
+    sb = new StringBuilder();
+    for (int i = sqSize - 1; i >= 0; i--) {
+      for (int j = sqSize - 1; j >= 0; j--) {
+        sb.append(sq[j][i]);
+      }
+    }
+    if (!sb.toString().equals(s))
+      return -1;
+    sb = new StringBuilder();
+    for (int i = 0; i < sqSize; i++) {
+      for (int j = 0; j < sqSize; j++) {
+        sb.append(sq[j][i]);
+      }
+    }
+    if (!sb.toString().equals(s))
+      return -1;
+    return sqSize;
+  }
+
+}
