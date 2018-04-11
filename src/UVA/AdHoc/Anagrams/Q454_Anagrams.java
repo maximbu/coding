08029d20969ffbde80394427492a9cddb1df9450
maximbu <maximbu@gmail.com>
2018-04-11 23:13:13 +0300

    Q454_Anagrams

---
+++ src/UVA/AdHoc/Anagrams/Q454_Anagrams.java
@@ -0,0 +1,73 @@
+package UVA.AdHoc.Anagrams;
+
+import static java.lang.System.in;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+class Q454_Anagrams {
+
+  public static void main(String[] st) {
+    Q454_Anagrams p = new Q454_Anagrams();
+    p.solve();
+  }
+
+  private void solve() {
+    Scanner sc = new Scanner(in);
+
+    int cases = sc.nextInt();
+    sc.nextLine();
+    sc.nextLine();
+    while (cases-- > 0) {
+      List<String> words = new ArrayList<>();
+      String word = sc.nextLine();
+      while (word.length() > 0) {
+        words.add(word);
+        if(!sc.hasNextLine()) break;
+        word = sc.nextLine();
+      }
+      Map<String, List<String>> ans = solve(words);
+      List<String> keys = ans.keySet().stream().sorted().collect(Collectors.toList());
+      for (String key : keys) {
+        List<String> vals = ans.get(key);
+        Collections.sort(vals);
+        for (String val : vals) {
+          System.out.println(key + " = " + val);
+        }
+
+      }
+      if (cases > 0) {
+        System.out.println();
+      }
+
+    }
+  }
+
+  private Map<String, List<String>> solve(List<String> words) {
+    Map<String, List<String>> ans = new HashMap<>();
+    Collections.sort(words);
+    List<String> sorted = new ArrayList<>();
+    for (String word1 : words) {
+      char[] ch = word1.toCharArray();
+      Arrays.sort(ch);
+      sorted.add(new String(ch).trim());
+    }
+    for (int i = 0; i < words.size(); i++) {
+      for (int j = i + 1; j < words.size(); j++) {
+        if (sorted.get(i).equals(sorted.get(j))) {
+          ans.putIfAbsent(words.get(i), new ArrayList<>());
+          ans.get(words.get(i)).add(words.get(j));
+        }
+      }
+    }
+    return ans;
+  }
+
+
+}
