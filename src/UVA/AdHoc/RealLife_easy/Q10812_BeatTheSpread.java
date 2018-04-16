0b1348026e13a31b1071c5af65e8516f6fcfc5cf
maximbu <maximbu@gmail.com>
2018-04-16 21:24:07 +0300

    Q10812_BeatTheSpread

---
+++ src/UVA/AdHoc/RealLife_easy/Q10812_BeatTheSpread.java
@@ -0,0 +1,33 @@
+package UVA.AdHoc.RealLife_easy;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+import java.util.Scanner;
+
+class Q10812_BeatTheSpread {
+
+  public static void main(String[] st) {
+    Q10812_BeatTheSpread p = new Q10812_BeatTheSpread();
+    p.solve();
+  }
+
+  private void solve() {
+    Scanner sc = new Scanner(in);
+    int cases = sc.nextInt();
+    while (cases-- > 0) {
+      sc.nextLine();
+      int sum = sc.nextInt();
+      int diff = sc.nextInt();
+      long b = diff+(sum-diff)/2;
+      long s = sum-b;
+      if((b-s)==diff && b >=0 && s>=0) {
+        out.println(b + " "+ s);
+      }else{
+        out.println("impossible");
+      }
+    }
+  }
+
+
+}
